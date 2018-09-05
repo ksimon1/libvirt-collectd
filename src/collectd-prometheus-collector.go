6f672235c3114ff37b12a223538b0a2282c051e7
Karel Simon <ksimon@redhat.com>
2018-09-05 08:37:20 +0200

    add function for removing last column from data

--- src/collectd-prometheus-collector.go
+++ src/collectd-prometheus-collector.go
@@ -12,14 +12,17 @@ import (
 
 func main() {
 	var (
-		collectdURL  string
-		fileLocation string
-		pollInterval int
+		collectdURL      string
+		fileLocation     string
+		pollInterval     int
+		removeLastColumn bool
 	)
 
 	flag.StringVar(&collectdURL, "collectdURL", "", "collectd URL *required")
 	flag.StringVar(&fileLocation, "fileLocation", "", "location of file to write in *required")
 	flag.IntVar(&pollInterval, "pollInterval", 15, "poll interval")
+	flag.BoolVar(&removeLastColumn, "removeLastColumn", true, "remove last column with timestamp")
+
 	flag.Parse()
 
 	if collectdURL == "" {
@@ -37,6 +40,10 @@ func main() {
 		}
 
 		if len(data) > 0 {
+			if removeLastColumn {
+				data = removeLastColumnFromData(data)
+			}
+
 			err = writeToFile(fileLocation, data)
 			if err != nil {
 				log.Println("could not write data to file: ", err.Error())
@@ -47,6 +54,7 @@ func main() {
 	}
 }
 
+//getPrometheusData retrieves prometheus data from server
 func getPrometheusData(collectdURL string) ([]byte, error) {
 	r, err := http.Get(collectdURL)
 	if err != nil {
@@ -58,6 +66,24 @@ func getPrometheusData(collectdURL string) ([]byte, error) {
 	return ioutil.ReadAll(r.Body)
 }
 
+//removeLastColumnFromData deletes timestamp from each line
+func removeLastColumnFromData(data []byte) []byte {
+	lines := strings.Split(string(data), "\n")
+
+	updatedLines := make([]string, len(lines))
+	for i, line := range lines {
+		//if line is empty or starts with # skip it
+		if line == "" || string(line[0]) == "#" {
+			updatedLines[i] = line
+			continue
+		}
+		updatedLines[i] = strings.TrimSuffix(line, line[strings.LastIndex(line, " "):])
+	}
+
+	return []byte(strings.Join(updatedLines, "\n"))
+}
+
+//writeToFile checks if file path exists and writes data to file
 func writeToFile(fileLocation string, prometheusData []byte) error {
 	pathToFile := strings.TrimSuffix(fileLocation, fileLocation[strings.LastIndex(fileLocation, "/"):])
 	if _, err := os.Stat(pathToFile); os.IsNotExist(err) {
