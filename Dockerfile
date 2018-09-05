70bbe13ed87f69b3a57ccd45e87bbbf39e76f223
Francesco Romani <fromani@redhat.com>
2018-09-05 14:03:41 +0200

    hook: autostart when libvirt starts
    With this change, we don't need anymore chnages in the images depending
    on this one.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,8 @@ COPY config/collectd.conf /etc/collectd.conf
 COPY config/collectd.d/*.conf /etc/collectd.d/
 
 COPY _output/bin/* /usr/bin/
+RUN mkdir -p /etc/libvirt/hooks
+COPY hook.sh /etc/libvirt/hooks/daemon
 
 EXPOSE 9090
 
---
+++ hook.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# expected call: 
+# /etc/libvirt/hooks/daemon - start - start
+if [ "$2" != "start" ] || [ "$4" != "start" ]; then
+	exit 0
+fi
+
+VM_NAME=$(hostname)
+
+# TODO: abort (and log) if fileLocation is not accessible - e.g. noone created the metrics-files subdirectory
+/usr/bin/collectd-prometheus-collector -collectdURL=http://localhost:9090/metrics -fileLocation=/var/run/kubevirt/metrics-files/${VM_NAME}.prom &
+/usr/sbin/collectd &
